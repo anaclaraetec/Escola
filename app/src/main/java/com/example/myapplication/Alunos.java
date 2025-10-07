484b57a7b28d88eb9d64ff9cd44fba4cb568868e
anaclaraetec2@gmail.com <anaclaraetec>
2025-10-07 20:22:14 -0300

    criado Classe alunos

---
+++ app/src/main/java/com/example/myapplication/Alunos.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+public class Alunos {
+
+    String nomeAt;
+    String cursoAt;
+
+    public Alunos(String namePar, String coursePar){
+        this.nomeAt = namePar;
+        this.cursoAt = coursePar;
+    }
+
+    public String getNome(){
+        return nomeAt;
+    }
+
+    public String getcuro(){
+        return cursoAt;
+    }
+
+
+}
